fc3828e9480abf36d4216c1211078c9730b0e9e6
magicdawn <magicdawn@qq.com>
2026-01-05 22:55:53 +0800

    chore: update tsconfig.json

--- tsdown.config.ts
+++ tsdown.config.ts
@@ -3,7 +3,7 @@ import { defineConfig } from 'tsdown'
 export default defineConfig({
   entry: ['src/index.ts'],
   format: 'esm',
-  target: 'node16',
+  target: 'es2022',
   clean: true,
   dts: true,
   fixedExtension: false, // false: use `.js`
